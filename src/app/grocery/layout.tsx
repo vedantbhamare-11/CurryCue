ec34c6cea85fd84a98c19719a4c98d30310e3b14
Vedant Bhamare <vedantdbhamare@gmail.com>
2025-05-31 23:53:53 +0530

    added grocery page

---
+++ src/app/grocery/layout.tsx
@@ -0,0 +1,7 @@
+export default function GroceryLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className="min-h-screen bg-background" data-unique-id="e09f44b7-482b-4232-a20b-0826eb0eb226" data-file-name="app/grocery/layout.tsx" data-dynamic-text="true">{children}</div>;
+}
